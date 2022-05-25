2c221fd5b6bf0866b32d468eae054e3c3306e29b
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-05-25 19:13:38 -0300

    feat(authenticate): refresh token function created

--- back-end/src/shared/infra/http/routes/authentication.routes.ts
+++ back-end/src/shared/infra/http/routes/authentication.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { AuthenticateUserController } from "@modules/authentication/use-cases/authenticate-user";
+import { RefreshTokenController } from "@modules/authentication/use-cases/refresh-token";
 
 
 const authenticateRoutes = Router()
 const authenticateUserController = new AuthenticateUserController()
+const refreshTokenController = new RefreshTokenController()
 
 authenticateRoutes.post('/sessions', authenticateUserController.handle)
+authenticateRoutes.post('/refresh-token', refreshTokenController.handle)
 
 export { authenticateRoutes }
