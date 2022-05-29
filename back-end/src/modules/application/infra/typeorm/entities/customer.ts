60b6bf0fe85345f62533df74966a0a3b1f94c587
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-05-29 16:00:03 -0300

    feat(back-end): customers database created

---
+++ back-end/src/modules/application/infra/typeorm/entities/customer.ts
@@ -0,0 +1,35 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm'
+import { v4 as uuidv4 } from 'uuid'
+
+
+@Entity('customers')
+class Customer {
+    @PrimaryColumn()
+    id?: string
+
+    @Column({ name: 'name', nullable: true })
+    name?: string
+
+    @Column({ name: 'email', nullable: true })
+    email?: string
+
+    @CreateDateColumn({ name: 'birth_date', nullable: true })
+    birthDate?: Date
+
+    @Column({ name: 'address', nullable: true })
+    address?: string
+
+    @CreateDateColumn({ name: 'created_at', nullable: true })
+    createdAt?: Date
+
+    @UpdateDateColumn({ name: 'updated_at', nullable: true })
+    updatedAt?: Date
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidv4()
+        }
+    }
+}
+
+export { Customer }
---
+++ back-end/src/shared/infra/typeorm/migrations/1653850486956-create-customer.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createCustomer1653850486956 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'customers',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'birth_date',
+                        type: 'timestamp',
+                    },
+                    {
+                        name: 'address',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('customers')
+    }
+
+}
