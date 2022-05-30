63f3e6d0c3054645160fe09fd5473fd6587ad01f
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-05-30 16:07:10 -0300

    fix(customer-backend): new AppError for invalid date format

--- back-end/src/modules/application/use-cases/create-customer/create-customer-use-case.ts
+++ back-end/src/modules/application/use-cases/create-customer/create-customer-use-case.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 
 import { ICustomerRepository } from "@modules/application/repositories";
 import { Customer } from "@modules/application/infra/typeorm/entities";
+import { AppError } from "@shared/errors/app-error";
 
 
 interface IRequest {
@@ -21,7 +22,15 @@ class CreateCustomerUseCase {
     async execute({ name, email, birthDateUnformatted, address }: IRequest): Promise<Customer> {
         const [day, month, year] = (birthDateUnformatted.split('/'))
 
-        const birthDate = new Date(Number(year), Number(month), Number(day))
+        const dayFull = Number(day)
+        const monthFull = Number(month)
+        const yearFull = Number(year)
+
+        if (!dayFull || !monthFull || !yearFull) {
+            throw new AppError('Invalid birth date', 401)
+        }
+
+        const birthDate = new Date(Number(year), Number(month) - 1, Number(day))
 
         const customer = await this.customerRepository.create({
             name,
